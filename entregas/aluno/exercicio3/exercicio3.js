c1e525abff0a6e7dcafdfc18eff056c3bfc44da4
AngelCosmic <ogecrom12@gmail.com>
2023-05-04 17:30:46 -0300

    Alteração exercicio 3

--- entregas/aluno/exercicio3/exercicio3.js
+++ entregas/aluno/exercicio3/exercicio3.js
@@ -3,6 +3,7 @@ let array = []
 function exibeNumero(entrada, mesmoNumero) {
     let espacamento = mesmoNumero;
     let contador = 1;
+    if (entrada, mesmoNumero > 0 && entrada, mesmoNumero <= 10) {
     for (let i = 1; i < entrada; i++) {
         array.push(" ".repeat(espacamento) + "#".repeat(contador) + " ".repeat(espacamento))
         if (espacamento > 0) { 
@@ -14,5 +15,8 @@ function exibeNumero(entrada, mesmoNumero) {
     for (const entrada of array) {
         console.log(entrada)
     }
+    } else {
+        console.log('Número Indisponivel');
+    }
 }
-exibeNumero(20, 20)
+exibeNumero(10, 10)
